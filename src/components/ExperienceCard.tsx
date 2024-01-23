038167c06f480acf5331c20986c6031a9b11a975
k1234906@kingston.ac.uk <k1234906@kingston.ac.uk>
2024-01-23 20:58:48 +0000

    Responsive mobile screen for Experience section

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,41 +1,23 @@
 // Import necessary libraries and components
-"use client";
 import React from 'react';
-import { motion } from 'framer-motion';
 import Image from 'next/image';
-
+import ExperienceCardContent from './ExperienceCardContent';
 type Props = {}
 
 // Define the ExperienceCard component
 function ExperienceCard({}: Props) {
   return (
     // Article container for the experience card
-    <article className=' bg-[#2f2f2f] flex flex-col mt-10 flex-shrink-0 rounded-lg items-center space-y-0  
+    <article className='border bg-[#2f2f2f] flex flex-col  flex-shrink-0 rounded-lg items-center space-y-0  
      py-17 sm:pt-10
-    h-[450px]      w-[200px] ml-[200px]
+    h-[200px]      w-[200px] ml-[200px]
     sm:w-[350px]  sm:h-[350px] 
     md:w-[600px]  md:h-[550px] 
     lg:w-[850px]  lg:h-[600px] lg:ml-[650px] 
-    xl:w-[1000px] xl:h-[580px] 
+    xl:w-[1000px] xl:h-[480px] 
     snap-center  p-2 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
       
-      {/* Display profile image with motion */}
-      <motion.img 
-        initial={{
-          y: -100, 
-          opacity: 0,
-        }}
-        transition={{ duration: 1.2 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        //viewport={{ once: true }}
-        className=' rounded-full 
-        w-20 h-20 
-        md:h-[140px] md:w-[140px]  
-        xl:w-[200px] xl:h-[200px] 
-        object-cover object-center'
-        src='https://scontent.flhr2-3.fna.fbcdn.net/v/t39.30808-6/404363941_7121455651226057_7843669369154568195_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=fjitpLFfEzUAX9q-BRQ&_nc_ht=scontent.flhr2-3.fna&oh=00_AfCW1Fy9jEYzWjOHr2QJuYy3cdQ7rI-YT2xP4O6Fcu0lCg&oe=65696200' 
-        alt=''
-      />
+      <ExperienceCardContent/>
       
       <div className='px-0 md:px-10 flex flex-col justify-between '>
         {/* Display job title */}
